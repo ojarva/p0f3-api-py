775b4d798afaf74022411d0bad769e32288c3df9
Olli Jarva <olli@jarva.fi>
2021-01-25 09:28:10 +0200

    Run isort & yapf

--- p0f/django/middleware.py
+++ p0f/django/middleware.py
@@ -14,17 +14,19 @@ to the same file.
 Start p0f with "-s /path/to/p0f_socket".
 """
 
+import logging
+import socket
+
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured, MiddlewareNotUsed
-import logging
+
 import p0f
-import socket
 
 log = logging.getLogger(__name__)
 
+
 class P0fMiddleware:
     """ Adds "p0f" attribute to request. Requires P0FSOCKET setting in Django settings.py """
-
     def __init__(self):
         try:
             enabled = settings.P0FENABLED
@@ -37,7 +39,9 @@ class P0fMiddleware:
             settings.P0FSOCKET
         except AttributeError:
             log.error("P0FSOCKET is not configured.")
-            raise ImproperlyConfigured("P0FSOCKET is not configured. This middleware does not run without path to p0f unix socket")
+            raise ImproperlyConfigured(
+                "P0FSOCKET is not configured. This middleware does not run without path to p0f unix socket"
+            )
 
     def process_request(self, request):
         remote_info = None
